167f4617d5a6aa768722c3a6684ccf908b087a6b
Sasha Schrandt <schrandt@gmail.com>
2023-04-30 01:32:00 -0500

    SECRET_KEY
    SECRET_KEY = 'YOUR_SECRET_KEY_GOES_HERE'

--- autolasercut/settings.py
+++ autolasercut/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+SECRET_KEY = 'YOUR_SECRET_KEY_GOES_HERE'
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
